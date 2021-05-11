3eae053f05fe1728e8c19008f3e986eeab206929
Nicholas-Stangl <78754216+Nicholas-Stangl@users.noreply.github.com>
2021-05-11 15:03:05 -0500

    first commit

---
+++ src/components/Poke.jsx
@@ -0,0 +1,44 @@
+import React, {useState, useEffect} from 'react';
+// import axios from 'axios'; need this for next one
+
+const Poke = () => {
+
+    const [allPokes, setAllPokes] = useState([])
+
+    const getPokies = (e)=>{
+        fetch("https://pokeapi.co/api/v2/pokemon/?limit=807")
+        .then(res=> {
+            return res.json()
+        })
+        .then(res => {
+            console.log("REEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE!")
+            console.log(res)
+            setAllPokes(res.results)
+
+        })
+        .catch(err => console.log("ERRROR", err))
+    }
+    
+    
+    
+    return (
+        <div>
+            <br/>
+            <br/>
+            <br/>
+            <button className="btn btn-secondary btn-lg" onClick={getPokies}>Fetch Pokemon</button>
+            <br/>
+            <br/>          
+            {
+                allPokes.map((poke,i) =>{
+                    return <div key = {i} >
+                        <h4>{poke.name}</h4>
+                </div>
+                })
+            }
+            
+        </div>
+    );
+};
+
+export default Poke;
